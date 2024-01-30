8fc7f115f611ef380583529c34a78fcac0c9c131
Ian Derrington <ianderrington@gmail.com>
2024-01-29 16:00:44 -0800

    update that is mostly working with sharable admonition

--- mkdocs-extensions/mkdocs-shareable-admonition/docs/index.md
+++ mkdocs-extensions/mkdocs-shareable-admonition/docs/index.md
@@ -2,8 +2,29 @@
 
 Here is a standard paragraph of text. Below is a shareable admonition.
 
-!!! note "This is a Note" shareable-note
+??? note "This is a Note" shareable-note
     This is the content of the note.
+
     It can span multiple lines.
 
 Here is another standard paragraph of text.
+
+!!! info "This is another Note" shareable-note2
+    This is the content of the note.
+
+    It can also span multiple lines.
+
+Here is another standard paragraph of text.
+
+??? info "This is another Note" shareable-note3
+    First question note
+
+    It can also span multiple lines.
+
+
+
+???+ info "This is another Note" shareable-note4
+    Second question note
+    
+    It can also span multiple lines.
+
--- mkdocs-extensions/mkdocs-shareable-admonition/src/shareable_admonition.py
+++ mkdocs-extensions/mkdocs-shareable-admonition/src/shareable_admonition.py
@@ -1,15 +1,14 @@
 import os
 import re
 import markdown
-import mkdocs
-from mkdocs.plugins import BasePlugin
-from mkdocs.structure.files import File
 import jinja2
 import tempfile
+from mkdocs.plugins import BasePlugin
+from mkdocs.structure.files import File
 
 class ShareableAdmonitionPlugin(BasePlugin):
-    # RE = re.compile(r'(?:^|\n)!!! ?([\w\-]+) +([\w\-]+) +"(.*?)" *(?:\n|$)')
-    RE = re.compile(r'(?:^|\n)(!!!|\?\?\?|\?\?\?\+)[ ]*([\w\-\+]+)[ ]+"([^"]+)"[ ]+([\w\-]+)(?:\n|$)')
+    # RE = re.compile(r'(?:^|\n)(!!!|\?\?\?|\?\?\?\+)[ ]*([\w\-\+]+)[ ]+"([^"]+)"[ ]+([\w\-]+)(?:\n|$)')
+    RE = re.compile(r'(?:^|\n)(!!!|\?\?\?|\?\?\?\+)\s*([\w\-\+]+)\s*"([^"]+)"\s+([\w\-]+)(?:\s|$)')
 
     TEMPLATE_NAME = 'admonition_template.html'
 
@@ -18,110 +17,84 @@ class ShareableAdmonitionPlugin(BasePlugin):
             loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates'))
         )
 
-    def on_files(self, files, config):
-        for file in files:
-            if file.src_path.endswith('.md'):
-                self.process_file(file, files, config)
-
-    def process_file(self, file, files, config):
-        if file.src_path.endswith('.temp.md'):
-            return
-        docs_dir = config['docs_dir']
-        site_dir = config['site_dir']
-        out_dir = file.url
-        with open(file.abs_src_path, 'r') as f:
-            content = f.readlines()
-
+    def on_page_markdown(self, markdown_content, page, config, files):
         new_content = []
         admonition_block = []
         in_admonition = False
         share_name = None
-        for line in content:
+        mkd_split = markdown_content.splitlines()
+        for line_num, line in enumerate(mkd_split):
+            # if line.startswith('???'):
             if self.RE.match(line):
+            # if line and line[:3] or line[:4] in types:
+                if line.startswith('???'):
+                    import ipdb; ipdb.set_trace()
                 in_admonition = True
                 admonition_type, tag, title, share_name = self.RE.match(line).groups()
+                new_line = ''.join([admonition_type, ' ', tag, ' "', title, '"'])
+                new_content.append(new_line) 
                 continue
-
+            
+            
             if in_admonition:
-                if line.startswith('    '):  # Content of the admonition
+                # end_block = False
+                if line.startswith('    '):
                     admonition_block.append(line[4:])  # Remove indentation
+                    end_block = False
+                elif line.startswith('\t'):
+                    admonition_block.append(line[1:])  # Remove indentation
+                    end_block = False
+                elif line == '':
+                    admonition_block.append(line)
+                    end_block = False
+                # elif line_num == len(mkd_split) - 1:
+                #     admonition_block.append(line)
+                #     end_block = True
                 else:
-                    in_admonition = False
-                    # Process the extracted admonition block
-                    # admonition_html_path = os.path.join(site_dir, f'{share_name}.html')
-                    admonition_html_path = os.path.join(out_dir, f'{share_name}.html')
+                    end_block = True
 
-                    # Create site_dir if it doesn't exist
-                    if not os.path.exists(admonition_html_path):
-                        os.makedirs(admonition_html_path)
-                    admonition_block = markdown.markdown(''.join(admonition_block), extensions=config['markdown_extensions']) 
-                    self.create_admonition_html(admonition_html_path,
-                        admonition_block, 
-                        title, 
-                        "Description", None, 
-                        page_url = f'{share_name}.html', 
-                        backlink_url=file.abs_src_path
-                    )
-                    iframe_html = self.create_iframe(share_name)
-                    new_content.append(iframe_html)
+                # elif line ==  "":  # Content of the admonition
+
+                if end_block:
+
+                    in_admonition = False
+                    # Process extracted admonition block
+                    admonition_content = '\n'.join(admonition_block)
+                    admonition_block_content = markdown.markdown(admonition_content, extensions=config['markdown_extensions'])
+                    shared_dir = os.path.join(config['site_dir'], 'shared')
+                    os.makedirs(shared_dir, exist_ok=True)
+
+                    admonition_html_path = os.path.join(shared_dir, f'{share_name}.html')
+                    print(f"admonition_html_path: {admonition_html_path}")
+                    if os.path.exists(admonition_html_path):
+                        raise Exception(f'File {path} already exists. You have a duplicate admonition share name: {share_name}')
+                    shareable_html_path = f'/shared/{share_name}.html'
+                    full_page_url = os.path.join('/', page.url, shareable_html_path)
+                    self.create_admonition_html(
+                        path=admonition_html_path, 
+                        content=admonition_block_content, 
+                        title=title, 
+                        description="Description", 
+                        image_url=None, 
+                        page_url=full_page_url, 
+                        # backlink_url=full_page_url)
+                        backlink_url=f'/{page.url}')
+
+
+                    iframe_html = self.create_iframe(os.path.join('/shared', f'{share_name}.html'))
+                 
+                    new_content.append('    '+iframe_html)
                     admonition_block = []
                     new_content.append(line)  # Include the current line as it's outside the admonition
             else:
                 new_content.append(line)
-        import ipdb; ipdb.set_trace()
 
-        # Create a temporary markdown file
-        temp_fd, temp_filepath = tempfile.mkstemp(suffix='.temp.md', dir=config['docs_dir'])
-
-        
-        with os.fdopen(temp_fd, 'w') as temp_file:
-            temp_file.writelines(new_content)
-
-        # Calculate relative path for the temp file from docs_dir
-        temp_rel_path = os.path.relpath(temp_filepath, config['docs_dir'])
-
-        # Replace the original file in MkDocs files list
-        temp_file_obj = File(path=temp_rel_path, src_dir=config['docs_dir'], dest_dir=config['site_dir'], use_directory_urls=config['use_directory_urls'])
-        files.append(temp_file_obj)
-
-        import ipdb; ipdb.set_trace()
-        # Optionally, update navigation to point to the temporary file
-        # ... (logic to update navigation)
-
-        return temp_filepath
+        return '\n'.join(new_content)
 
 
-    # def on_nav(self, nav, config, files):
-    #     temp_files_map = self.create_temp_files_map(files)
-    #     self.update_nav(nav, temp_files_map)
-
-    # def create_temp_files_map(self, files):
-    #     # Mapping of original file paths to temporary file paths
-    #     temp_files_map = {}
-    #     for file in files:
-    #         if file.abs_src_path.endswith('.tmp.md'):  # Assuming temporary files end with '.tmp.md'
-    #             original_path = file.abs_src_path.replace('.tmp.md', '.md')
-    #             temp_files_map[original_path] = file.abs_src_path
-    #     return temp_files_map
-
-    # def update_nav(self, nav, temp_files_map):
-    #     if not nav or not nav.items:
-    #         return
-
-    #     for item in nav.items:
-    #         if isinstance(item, mkdocs.nav.Section):
-    #             self.update_nav(item, temp_files_map)  # Recursive call for sections
-    #         elif isinstance(item, mkdocs.nav.Page) and item.file.abs_src_path in temp_files_map:
-    #             item.file.abs_src_path = temp_files_map[item.file.abs_src_path]
-    #             item.file.src_path = os.path.basename(temp_files_map[item.file.abs_src_path])
-
-
-        
     def create_iframe(self, share_name):
-        iframe_html = f'<iframe src="{share_name}.html" width="100%" height="100%"></iframe>\n'
+        iframe_html = f'<iframe src="{share_name}" width="100%" height="100%" frameBorder="0" loading="lazy"></iframe>\n'
         return iframe_html
-
-
 
     def create_admonition_html(self, path, content, title, description, image_url, page_url, backlink_url):
         template = self.template_env.get_template(self.TEMPLATE_NAME)
@@ -136,9 +109,5 @@ class ShareableAdmonitionPlugin(BasePlugin):
         with open(path, 'w') as f:
             f.write(html_content)
 
-
-
     def create_share_button(self, share_name, title):
         return f'<button onclick="window.open(\'{share_name}.html\', \'_blank\')">{title}</button>\n'
-
-    # Other necessary plugin methods...
