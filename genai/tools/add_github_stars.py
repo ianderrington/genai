fe82998c03c947a6dcebdd4934d4352782107f2a
Ian Derrington <ianderrington@gmail.com>
2024-03-29 06:11:11 -0700

    change to github star

--- genai/tools/add_github_stars.py
+++ genai/tools/add_github_stars.py
@@ -4,17 +4,43 @@ import re
 
 def replace_github_links_in_file(file_path):
     with open(file_path, 'r+', encoding='utf-8') as file:
-        content = file.read()
+        # content = file.read()
+        # if not content.startswith('???') or not content.endswith('!!!'):
+        ensure_starts_with = ['???', '!!!']
         github_link_pattern = r'\[([^\]]+)\]\(https://github\.com/([^/]+/[^/]+)\)'
-        
+        do_not_match = '![GitHub Repo stars]'
+        # Go line by line ensure it starts with the expected strings
+        # Ensure it doesn't have the do_not_match string
+        # Replace any github links with the extended version
         def replacement(match):
             repo_name = match.group(2)
-            return f'[GitHub Repo stars](https://badgen.net/github/stars/{repo_name})  [{match.group(1)}](https://github.com/{repo_name})'
+            repo_base= f'https://github.com/{repo_name}'
+            # ensure do_not_match is not in the content
+            if do_not_match not in content:
+                return f'![GitHub Repo stars](https://badgen.net/github/stars/{repo_name})  [{match.group(1)}]' + f'({repo_base})'
 
-        updated_content = re.sub(github_link_pattern, replacement, content)
+   
+        # readlines
+        new_lines = []
+        lines = file.readlines()
+        for line in lines:
+            if not line.startswith('???') or not line.endswith('!!!'):
+                pass 
+            elif do_not_match in line:
+                pass 
+            else:
+                line = re.sub(github_link_pattern, replacement, line)
+            new_lines.append(line)
         file.seek(0)
-        file.write(updated_content)
+        file.write(''.join(new_lines))
         file.truncate()
+
+
+
+     # updated_content = re.sub(github_link_pattern, replacement, content)
+        # file.seek(0)
+        # file.write(updated_content)
+        # file.truncate()
 
 def crawl_directory_and_replace_links(directory):
     for root, dirs, files in os.walk(directory):
